f3a3feb39df5a91c6936c340753b87a12174ff19
#Nick <nicknessuno001@gmail.com>
2018-03-27 15:11:34 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -4,3 +4,5 @@ if (!isset($msg)) return 0;
 if ($msg == '/info') {
   sm($chatID, "<b>Info chat:</b>\nID: $chatID\nTitolo: $title\nUsername chat: @$chatusername\nTipo: $type\n\n<b>Informazioni utente:</b>\nID: $userID\nNome: $name\nUsername: @$username", 1);
 }
+
+aked
